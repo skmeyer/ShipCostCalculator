4f2531b05fb3f7f0e57d0e0129e69900e00bc6d4
skmeyer <sarahbutwritingeditioN@gmail.com>
2025-02-04 16:57:48 -0500

    Complete

--- src/ShipCostCalculator.java
+++ src/ShipCostCalculator.java
@@ -5,5 +5,22 @@ public class ShipCostCalculator
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
+        double itemPrice = 0;
+        double shippingCost = 0;
+        double totalPrice = 0;
+
+        System.out.print("Enter the price of your item: ");
+        itemPrice = in.nextDouble();
+        if(itemPrice >= 100)
+        {
+            System.out.println("Your shipping is free!");
+        }
+        else
+        {
+            shippingCost = itemPrice * 0.02;
+            System.out.println("Your shipping is " + shippingCost + " dollars.");
+        }
+        totalPrice = itemPrice + shippingCost;
+        System.out.println("Your total price is " + totalPrice + " dollars.");
     }
 }
